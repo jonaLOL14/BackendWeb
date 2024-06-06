fa853d73e696029863a8cc6a49639eb024848513
rojo1994 <rodri-1994-s2@hotmail.com>
2024-06-05 18:08:52 -0700

    fix

--- src/config/db.js
+++ src/config/db.js
@@ -4,12 +4,7 @@ const URI = "mongodb+srv://beljona27:7oHRPwPC2daqcaHQ@proyectofinal.wdun4by.mong
 const dbConnection = async () => {
     console.log('Intentando conectar a MongoDB...');
     try {
-        await mongoose.connect(URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-        });
+        await mongoose.connect(URI);
         console.log('MongoDB connected...');
     } catch (error) {
         console.error('Error connecting to MongoDB: ', error);
